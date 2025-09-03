6302a730b40871375758f77eb181c328fdb8c179
lala <garciagamezjuandiego0@gmail.com>
2025-09-03 13:15:30 -0500

    Proceso de resta y multiplicacion

--- Menu.py
+++ Menu.py
@@ -1,7 +1,27 @@
+def resta (num1,num2 ):
+    resta = num1-num2
+    return resta
+    
+def multiplicacion(num1, num2):
+    multiplicacion = num1*num2
+    return round(multiplicacion,2)
+
+
+
 print ("Calculadora")
 print ("1. Suma")
 print ("2. Resta")
 print ("3. Multiplicacion")
 print ("4. divicion")
 proceso=input("Que opcion deseas realizar (1-4): ")
+
+if proceso=="2":
+    num1=float(input("Digite primer numero resta: "))
+    num2=float(input("Digite segundo numero resta: "))
+    print("Su resta es ",resta(num1, num2))
+
+if proceso=="3":
+    num1=float(input("Digite numero de multiplicaicon: "))
+    num2=float(input("Digite numero de multiplicaicon: "))
+    print("Su resultado de multiplicacion es: ",multiplicacion(num1,num2))
  
